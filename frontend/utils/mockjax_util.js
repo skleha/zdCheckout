929f8a3d74efca60e12489f3ae61b94cf74b2357
thejenkdog <thejenkdog@gmail.com>
2020-02-26 15:24:37 -0800

    Add plan information and a mockjax api/current PUT endpoint

--- frontend/utils/mockjax_util.js
+++ frontend/utils/mockjax_util.js
@@ -1,11 +1,50 @@
 
+const PLAN_COSTS = {
+  basic: 1,
+  good: 10,
+  better: 100,
+  best: 1000
+};
+
+const PLAN_NAMES = {
+  basic: "Basic",
+  good: "Good",
+  better: "Better",
+  best: "Best"
+};
+
+let prevSubscription;
+
+let currSubscription = {
+  plan: "good",
+  name: "Good",
+  seats: 5,
+  cost: 50
+};
+
+
 $.mockjax({
   url: "/api/current",
   type: "GET",
-  responseText: {
-    status: 'success',
-    post: 'hello world'
-  }
+  responseText: currSubscription
 });
 
 
+$.mockjax({
+  url: "/api/current",
+  type: "PUT",
+  responseDelay: 1000,
+  response: function(settings) {
+    
+    let newData = {
+      plan: settings.data.plan,
+      name: PLAN_NAMES[settings.data.plan],
+      seats: settings.data.seats,
+      cost: settings.data.seats * PLAN_COSTS[settings.data.plan]
+    };
+
+    prevSubscription = currSubscription;
+    currSubscription = newData;
+    this.responseText = newData;
+  }
+});
