557de200ade5e81203d2329d40ad55836cf4955a
thejenkdog <thejenkdog@gmail.com>
2020-02-26 13:36:10 -0800

    Add text to index.html, edit webpack config, add logic to entry file

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   context: __dirname,
   entry: "./frontend/checkout.jsx",
   output: {
-    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    path: path.resolve(__dirname),
     filename: "bundle.js"
   },
   resolve: {
