d29dd505334ffb4baf3bbf776a49dac0d28a2ffe
thejenkdog <thejenkdog@gmail.com>
2020-02-26 13:57:00 -0800

    Add logic to Root component

---
+++ frontend/components/Root.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
+
+const Root = () => {
+
+  <div>You've got the root</div>
+
+}
+
+export default Root;
+
