0384817df90714b30be6d9df187ee3c9f7980351
thejenkdog <thejenkdog@gmail.com>
2020-02-27 20:34:07 -0800

    First draft of the confirm page

---
+++ frontend/components/Confirm.jsx
@@ -0,0 +1,19 @@
+
+
+class Confirm extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+      <div>You've got the confirm page</div>
+    )
+  }
+
+}
+
+
+export default Confirm;
---
+++ frontend/components/ConfirmContainer.js
@@ -0,0 +1,13 @@
+
+import { connect } from 'react-redux';
+import Confirm from './Confirm';
+
+const mapStateToProps = state => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Confirm);
